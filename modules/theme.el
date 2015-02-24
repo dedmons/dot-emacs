6533ad4ea35c0f774d001fd604a5c8824131bf1c
DJ Edmonson <djedmonson@gmail.com>
2015-02-23 22:35:05 -0800

    update theme

--- modules/theme.el
+++ modules/theme.el
@@ -1,8 +1,9 @@
 (defvar my-theme-package 'color-theme-solarized)
-(defvar my-theme 'solarized-dark)
+(defvar my-theme 'solarized)
+
+(set-frame-parameter nil 'background-mode 'dark)
 
 (when window-system
     (when (not (package-installed-p my-theme-package))
       (package-install my-theme-package))
-    (load-theme 'solarized-dark t))
-			  
+    (load-theme 'solarized t)) 
